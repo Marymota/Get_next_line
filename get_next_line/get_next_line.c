7609811558e445c01de4d6795a164c948996d0df
marymota <dg.marciamota@gmail.com>
2021-03-12 16:24:47 +0000

    works with short buffers

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -2,7 +2,7 @@
 
 int get_next_line (int fd, char **line)
 {
-	int BUFFER_SIZE = 10;
+	int BUFFER_SIZE = 33;
 	static char *store[1024];
 	char buf[BUFFER_SIZE + 1];
 	int bytes_read;
@@ -12,52 +12,53 @@ int get_next_line (int fd, char **line)
 	int i;
 	int j;
 
-	//printf("buf_tmp: %s\n", buf_tmp);
+
 	if (fd < 0 || line == NULL)
 		return (-1);
+
 	if (store[fd] != NULL && i > 0)
-		ft_strdel(&store[fd]);
+		free(store[fd]);
 
 	if (buf_tmp != NULL)
 		store[fd] = ft_strdup(buf_tmp);
 
-	while (!(ft_strchr(buf, '\n')))
+	while (bytes_read && !(ft_strchr(buf, '\n')))
 	{
 		bytes_read = read(fd, buf, BUFFER_SIZE);
 		buf[bytes_read] = '\0';
-		if (store[fd] == NULL)
-		{
+		if (store[fd] == NULL && bytes_read)
 			store[fd] = ft_strdup(buf);
-		}
-		else if (!(ft_strchr(buf, '\n')))
+		else if (!(ft_strchr(buf, '\n')) && bytes_read)
 		{
 			tmp = ft_strjoin(store[fd], buf);
-			ft_strdel(&store[fd]);
+			free(store[fd]);
 			store[fd] = tmp;
 		}
 		else
 		{
 			i = 0;
-			buf_tmp = malloc(sizeof(char) * BUFFER_SIZE);
+			buf_tmp = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 			while (buf[i] != '\n')
 			{
 				extra[i] = buf[i];
 				i++;
 			}  
-			extra[++i] = '\0';
+			extra[i++] = '\0';
 			j = 0;
 			while (i < BUFFER_SIZE)
-			{
 				buf_tmp[j++] = buf[i++];
-			}
 			buf_tmp[j] = '\0';
 			tmp = extra;
 			store[fd] = ft_strjoin(store[fd], tmp);		
-		}
+		}		
+		*line = store[fd];
 	}
-	if (line == NULL && (store[fd] == NULL && bytes_read == 0 && buf == 0))
+	if (!bytes_read && !*store[fd])
+	{
+		free(store[fd]);
+		*line = NULL;
 		return (0);
-	*line = store[fd];
+	}
 	return (1);
 }
 
@@ -65,18 +66,12 @@ int main (void)
 {
 	int 	fd;
 	char	*line;
+	int ret;
 
 	fd = open("text.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
+	while ((ret = get_next_line(fd, &line)) == 1)
+	{
+		printf("%s\n", line);
+	}
+	return (0);
 }
